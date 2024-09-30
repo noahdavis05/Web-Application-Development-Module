568a3a60dbf05f5eae0e11207ee4014cfd20d498
noahdavis05 <146333718+noahdavis05@users.noreply.github.com>
2024-09-30 21:37:39 +0100

    Added a working database to the project where you can save property detaisl to

--- Section_2/app/forms.py
+++ Section_2/app/forms.py
@@ -1,7 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import IntegerField
+from wtforms import IntegerField, StringField, DateField
 from wtforms.validators import DataRequired
 
 class CalculatorForm(FlaskForm):
     number1 = IntegerField('number1', validators=[DataRequired()])
     number2 = IntegerField('number2', validators=[DataRequired()])
+    
+    
+class PropertyForm(FlaskForm):
+    address = StringField('address', validators=[DataRequired()])
+    start_date = DateField('start_date', validators=[DataRequired()])
+    duration = IntegerField('duration', validators=[DataRequired()])
+    rent = IntegerField('rent', validators=[DataRequired()])
--- Section_2/app/views.py
+++ Section_2/app/views.py
@@ -1,6 +1,8 @@
-from flask import render_template, flash
+from flask import render_template, flash, redirect, url_for
 from app import app
 from .forms import *
+from .models import Property
+from app import db
 
 # The main webpage
 @app.route('/')
@@ -23,3 +25,26 @@ def calculator():
     return render_template('calculator.html',
                            title='Calculator',
                            form=form)
+ 
+  
+    
+@app.route('/property', methods=['GET', 'POST'])
+def property():
+    form = PropertyForm()
+    if form.validate_on_submit():
+        # Create a new Property instance from form data
+        new_property = Property(
+            address=form.address.data,
+            start_date=form.start_date.data,  # Assuming this is a DateField
+            duration=form.duration.data,
+            rent=form.rent.data
+        )
+        
+        # Add the new property to the database
+        db.session.add(new_property)
+        db.session.commit()  # Save the changes to the database
+        
+        flash('Successfully received and saved form data.')
+        return redirect(url_for('property'))  # Redirect after successful submission
+    
+    return render_template('property.html', title='Property', form=form)
