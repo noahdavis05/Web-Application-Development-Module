da1e5a6b59c4c52032df916acde41a4b1613d2b5
noahdavis05 <146333718+noahdavis05@users.noreply.github.com>
2024-09-30 18:23:31 +0100

    Updated my app to have two pages, completed task 3

--- Section_2/app/views.py
+++ Section_2/app/views.py
@@ -1,5 +1,15 @@
+from flask import render_template
 from app import app
 
+# The main webpage
 @app.route('/')
 def index():
-    return "Hello World!!!"
+    user = {'name': 'Homer Simpson'}
+    return render_template('index.html',
+                           title='Simple template example',
+                           user=user)
+    
+@app.route('/fruit')
+def displayFruit():
+    fruits = ["Apple", "Banana", "Orange", "Kiwi"]
+    return render_template("fruit.html",fruits=fruits)
