1add9b58d2d4887f5e781048d39d55ef4c84af25
noahdavis05 <146333718+noahdavis05@users.noreply.github.com>
2024-09-30 21:07:19 +0100

    Finished the next stage of the course

--- Section_2/app/views.py
+++ Section_2/app/views.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash
 from app import app
+from .forms import *
 
 # The main webpage
 @app.route('/')
@@ -13,3 +14,12 @@ def index():
 def displayFruit():
     fruits = ["Apple", "Banana", "Orange", "Kiwi"]
     return render_template("fruit.html",fruits=fruits)
+
+@app.route('/calculator', methods=['GET', 'POST'])
+def calculator():
+    form = CalculatorForm()
+    if form.validate_on_submit():
+        flash('Succesfully received form data. %s + %s  = %s'%(form.number1.data, form.number2.data, form.number1.data+form.number2.data))
+    return render_template('calculator.html',
+                           title='Calculator',
+                           form=form)
